b0d5a5b3f2d111251ebc1a73c7aec50315b74b76
SoeunLee <soeun2327@gmail.com>
2017-08-04 15:41:47 +0900

    날씨: 과거 날씨 또는 월별 마지막 날 날씨만 나타내기

--- server/weather.py
+++ server/weather.py
@@ -1,42 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import calendar
 
-def getWeather(year, mm, stn='108'):
-    urls = ["http://www.kma.go.kr/weather/climate/past_cal.jsp?stn={0}&yy={1}&mm={2}&obs=1".format(stn, year, mm),
-            "http://www.kma.go.kr/weather/climate/past_cal.jsp?stn={0}&yy={1}&mm={2}&obs=9".format(stn, year, mm)]
+
+def weather(year: int, month: int, stn=108):
+    # stn=stn(지역), &yy=year(년), &mm=month(월) <= 모두 int
+
+    urls = ["http://www.kma.go.kr/weather/climate/past_cal.jsp?stn={0}&yy={1}&mm={2}&obs=1".format(stn, year, month),
+            "http://www.kma.go.kr/weather/climate/past_cal.jsp?stn={0}&yy={1}&mm={2}&obs=9".format(stn, year, month)]
     # urls[0](기온/강수량), urls[1](날씨)
 
-    # stn=stn(지역), &yy=year(년), &mm=mm(월)
     response = requests.get(urls[1])
-
     dom = BeautifulSoup(response.text, "html.parser")
     elements = dom.select("tbody tr")
 
+    yy = datetime.today().year
+    mm = datetime.today().month
+    dd = datetime.today().day
+    end_day = calendar.monthrange(year, month)[1]
+
+    if year > yy or (year == yy and month > mm):
+        return
+
     skip = True
     cnt = 0
-    # date = 0
+    day = 0
+
     for element in elements:
-        # TODO: 오늘 날짜 가져와서 이전 날짜만 출력하기
-        # TODO: date 변수로 날짜도 출력하기
         tds = element.select("td")
 
         for td in tds:
-            if skip:
+            if month >= mm and day >= dd - 1 or day >= end_day:
+                break
+            elif skip:
                 if td.text == " ":
-                    cnt = cnt+1
-                continue
-            if td.text == " " and cnt > 0:
-                cnt = cnt-1
+                    cnt = cnt + 1
+            elif td.text == " " and cnt > 0:
+                cnt = cnt - 1
             elif td.text != " ":
+                day = day + 1
                 for br in td.find_all("br"):
                     br.replace_with("\n")
-                print(td.text + "\n")
+                print(str(day) + '. ' + td.text + "\n")
             else:
-                print('맑음\n')
+                day = day + 1
+                print(str(day) + '. 맑음\n')
 
         if skip:
             skip = False
         else:
             skip = True
 
-getWeather('2017', '7')
+
+weather(2017, 8)
