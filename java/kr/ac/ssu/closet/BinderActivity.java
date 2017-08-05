6ced3b73eaf9ea4dbf493494963d9c7cf55633a0
SoeunLee <soeun2327@gmail.com>
2017-08-05 18:05:45 +0900

    xml 수정: 색상 수정, 탭 선택 시 탭 하단 선 출력, 가로 크기 줄어듦

--- java/kr/ac/ssu/closet/BinderActivity.java
+++ java/kr/ac/ssu/closet/BinderActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -24,28 +25,43 @@ public class BinderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_binder);
+        getSupportActionBar().hide();
 
         rgTab = (RadioGroup) findViewById(R.id.group_tab);
 
         tab[0] = (RadioButton) findViewById(R.id.tab_today);
         tab[1] = (RadioButton) findViewById(R.id.tab_calendar);
         tab[2] = (RadioButton) findViewById(R.id.tab_closet);
-
-        tab[0].setChecked(true);
-
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.frame, new TodayFragment());
-        fragmentTransaction.commit();
+//
+//        FragmentManager fragmentManager = getFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.frame, new TodayFragment());
+//        fragmentTransaction.commit();
 
         rgTab.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             Fragment fragment;
+            LinearLayout.LayoutParams wide = new LinearLayout.LayoutParams(0, 150, 3);
+            LinearLayout.LayoutParams narrow = new LinearLayout.LayoutParams(0, 150, 2);
             private final int[] tabId = {tab[0].getId(), tab[1].getId(), tab[2].getId()};
+
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == tabId[1]) fragment = new CalendarFragment();
-                else if(checkedId == tabId[2]) fragment = new ClosetFragment();
-                else fragment = new TodayFragment();
+
+                tab[1].setLayoutParams(narrow);
+                tab[2].setLayoutParams(narrow);
+                tab[0].setLayoutParams(narrow);
+
+                if(checkedId == tabId[1]) {
+                    fragment = new CalendarFragment();
+                    tab[1].setLayoutParams(wide);
+                } else if(checkedId == tabId[2]) {
+                    fragment = new ClosetFragment();
+                    tab[2].setLayoutParams(wide);
+                } else {
+                    fragment = new TodayFragment();
+                    tab[0].setLayoutParams(wide);
+                }
+
 
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -53,5 +69,6 @@ public class BinderActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
             }
         });
+        tab[0].setChecked(true);
     }
 }
